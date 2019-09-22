fc424ccebe6518ad3694f99f2c29625a0da1d693
Gianluca-L <gluca1993.gl@gmail.com>
2019-09-22 15:03:15 +0200

    4 seconds

--- sketch.js
+++ sketch.js
@@ -1,65 +1,84 @@
 function preload(){
   // put preload code here
+
 }
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth, windowHeight);
+  background("black");
   angleMode(DEGREES);
   frameRate(60);
+  r = width/6;
+
+  s = 60; // 1 second
 
 }
 
 function draw() {
-  // put drawing code here
-  //translate(width/2, height/2);
-  //line(width/4, height/2, (3*width)/4, height/2);
-  /*translate((3*width)/4, height/2);
-  rotate (frameCount*3)
-
-
-  line(-frameCount*10, frameCount*10, 0, 0);
-  strokeWeight(2);
-
-  if (frameCount == 60) {
-    noLoop();
-  }*/
-  strokeWeight(1.5);
-  translate((3*width)/4.25, height/2);
 
 
 
-  stroke("red");
-  line(0, 0, -(width/5)*(1-cos(frameCount*1)), (width/5)*sin(frameCount*1));
+  //background("black");
 
-  stroke("orange");
-  line(0, 0, -(width/5)*(1-cos(frameCount*1.2)), (width/5)*sin(frameCount*1.2));
+
+  translate(2*width/3, height/2);
+
+
+  //stroke("red");
+  //line(-r*(1-cos(frameCount*1)), r*sin(frameCount*1), 0, 0);
+
+  /*stroke("orange");
+  line(-r*(1-cos(frameCount*1.2)), r*sin(frameCount*1.2), 0, 0);
 
   stroke("yellow");
-  line(0, 0, -(width/5)*(1-cos(frameCount*1.4)), (width/5)*sin(frameCount*1.4));
+  line(-r*(1-cos(frameCount*1.4)), r*sin(frameCount*1.4), 0, 0);
 
   stroke("green");
-  line(0, 0, -(width/5)*(1-cos(frameCount*1.6)), (width/5)*sin(frameCount*1.6));
+  line(-r*(1-cos(frameCount*1.6)), r*sin(frameCount*1.6), 0, 0);
 
-  stroke(0, 255, 255)
-  line(0, 0, -(width/5)*(1-cos(frameCount*1.8)), (width/5)*sin(frameCount*1.8));
+  stroke(0, 255, 255);
+  line(-r*(1-cos(frameCount*1.8)), r*sin(frameCount*1.8), 0, 0);
 
   stroke("blue");
-  line(0, 0, -(width/5)*(1-cos(frameCount*2.1)), (width/5)*sin(frameCount*2.1));
+  line(-r*(1-cos(frameCount*2.1)), r*sin(frameCount*2.1), 0, 0);
 
   stroke("violet");
-  line(0, 0, -(width/5)*(1-cos(frameCount*2.3)), (width/5)*sin(frameCount*2.3));
+  line(-r*(1-cos(frameCount*2.3)), r*sin(frameCount*2.3), 0, 0);*/
+
+
+  //background("black");
+
+  if (frameCount > 2*s && frameCount < 4*s ) {
+    strokeWeight(2.5);
+    stroke("black");
+    line(0, 0, -r*(1-cos(frameCount*3)), r*sin(frameCount*3));
+  };
+  if (frameCount < 2*s) {
+
+    strokeWeight(1.5);
+    stroke("green");
+    line(0, 0, -r*(1-cos(frameCount*3)), r*sin(frameCount*3));
+  };
+
+
+  /*stroke("green");
+  strokeWeight(1.5);
+  for (i = 0; i < 360; i+=2) {
+    line(0, 0, -r*(1-cos(i)), r*sin(i))
+  };*/
 
 
 
 
-  let a = 0.0;
-  let inc = 2;
-  for (let i = 0; i < 180; i++) {
-    stroke("black")
-    line(0, 0, -(width/5)*(1-cos(a)), (width/5)*sin(a));
-    a = a + inc;
-  }
+
+
+
+
+
+
+
+
 
 
 
